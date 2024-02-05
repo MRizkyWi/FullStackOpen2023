6b920ddcbce7364577de5f006b93b81832e7a230
MRizkyWi <rizky.widyayulianto@gmail.com>
2024-02-05 14:56:33 +0700

    exercise 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,8 @@ morgan.token('body', req => {
 })
 app.use(express.json())
 app.use(morgan(':method :url :body'))
+
+app.use(cors())
 
 let persons = [
   { 
